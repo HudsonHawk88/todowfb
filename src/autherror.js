058c4320c1b3708b2637feb5050a93a31b8a15ab
Tóth Gergő <tothg@zalaszam.local>
2019-07-12 15:15:46 +0200

    kesz

--- src/autherror.js
+++ src/autherror.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import {Spinner} from 'reactstrap';
 
 class Error extends Component{
 render(){
     return(
-        <div></div>
+        <div><Spinner color="primary" /></div>
         
     )
 }
