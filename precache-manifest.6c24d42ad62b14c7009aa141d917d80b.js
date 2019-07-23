8649e4111c886b5669aeb5139c663069567a58b9
Tóth Gergő <tothg@zalaszam.local>
2019-07-23 15:53:14 +0200

    Updates

---
+++ precache-manifest.6c24d42ad62b14c7009aa141d917d80b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db4063ba4ed1a39d27ebabfe0375dde",
+    "url": "/todowfb/index.html"
+  },
+  {
+    "revision": "b1d12b519fcbbff8e266",
+    "url": "/todowfb/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "8cd90794841b5b36af83",
+    "url": "/todowfb/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b1d12b519fcbbff8e266",
+    "url": "/todowfb/static/js/2.1ac4afc9.chunk.js"
+  },
+  {
+    "revision": "8cd90794841b5b36af83",
+    "url": "/todowfb/static/js/main.7d846b59.chunk.js"
+  },
+  {
+    "revision": "ca7155f01b95ffde5b64",
+    "url": "/todowfb/static/js/runtime~main.15e6ff38.js"
+  }
+]);
